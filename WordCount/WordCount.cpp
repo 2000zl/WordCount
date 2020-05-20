5abf923fc4b5b9235ad6224cee45eeda5a20edc1
ZL <1287339605@qq.com>
2020-05-20 13:28:37 +0800

    newSubmit

--- WordCount/WordCount.cpp
+++ WordCount/WordCount.cpp
@@ -5,29 +5,10 @@
 
 int main(int argc, char* argv[])
 {
-
-	/*
-	 printf("main函数收到了%d个参数\n",argc);//第一个参数默认是可执行文件本身，第二个参数开始是接受到的参数
-	   int i;
-	  for( i = 0; i < argc; i++ ){
-		printf("%s ",argv[i]);
-	  }
-	   printf("\n");
-	  */
-
+	//main函数的第一个参数默认是可执行文件本身，
+	//第二个参数开始是接收到的参数。
 	if (argc == 3) {
-
-		//打印接受到的参数
-		/*
-		 int i;
-		 printf("收到的外部参数：");
-		 for( i = 1; i < argc; i++ ){
-		  printf("%s ",argv[i]);
-		 }
-		 printf("\n");
-		  */
-
-		  //尝试读取文件的内容
+		//读取文件的内容
 		char data;
 		FILE* fp = fopen(argv[2], "r");
 
@@ -38,22 +19,21 @@ int main(int argc, char* argv[])
 		}
 
 		//对参数进行判断
-
-		if (!strcmp(argv[1], "-c")) {//字符数
-			char temp;
+		if (!strcmp(argv[1], "-c")) {
+			//字符数
+			char ch;
 			int num = 0;
-			temp = fgetc(fp);
-			while (temp != EOF) {
-				//putchar(temp);
-				temp = fgetc(fp);
+			ch = fgetc(fp);
+			while (ch != EOF) {
+				ch = fgetc(fp);
 				num++;
-
 			}
-			printf("该文本文件的字符数为：%d\n", num);
-
-
+			printf("字符数：%d\n", num);
 		}
-		else if (!strcmp(argv[1], "-w")) {//单词数
+
+		//对参数进行判断
+		else if (!strcmp(argv[1], "-w")) {
+			//单词数
 			int w = 0;
 			char a = fgetc(fp);
 			while (a != EOF) {
@@ -62,48 +42,22 @@ int main(int argc, char* argv[])
 						a = fgetc(fp);
 					w++;
 					a = fgetc(fp);
-
-
 				}
 				else
 					a = fgetc(fp);
-
 			}
-			printf("该文本文件的单词数为：%d\n", w);
-
-
+			printf("单词数：%d\n", w);
 		}
-		else if (!strcmp(argv[1], "-l")) {//行数
-			char temp;
-			int num = 1;
-			temp = fgetc(fp);
-			while (temp != EOF) {
-
-				//putchar(temp);
-				if (temp == '\n') {//字符需要用单引号扩住
-					num++;
-
-				}
-				temp = fgetc(fp);
-
-
-
-			}
-			printf("该文本文件的行数为：%d\n", num);
-
-
-		}
+		
 		else {
 			printf("错误：参数超出范围！\n");
-			printf("允许参数：-c，-w，-l\n");
-
+			printf("允许参数：-c，-w\n");
 		}
 		int n = fclose(fp);
-
 	}
 	else {
 		printf("错误：运行参数必须要3个\n");
-		printf("用法：wc.exe [parameter] [file_name]\n");
+		printf("用法：wc.exe [parameter] [input_file_name]\n");
 
 	}
 	system("pause");
